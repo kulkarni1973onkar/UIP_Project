37069ed842663cd1108acdd3daec2ecbec17c066
Onkar Kulkarni <91174083+kulkarni1973onkar@users.noreply.github.com>
2024-06-15 00:30:36 +0530

    Updated commit

---
+++ Server/Models/Book.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+
+const bookSchema = new mongoose.Schema({
+    title: String,
+    author: String,
+    publisher: String,
+    totalPrice: [String],
+    isbn: [String],
+    userId:{type:mongoose.Schema.Types.ObjectId,ref:'user'}
+});
+
+
+const Book = mongoose.model("Book", bookSchema);
+
+
+async function createBook(userId,title, author, publisher, totalPrice, isbn) {
+    const newBook = await Book.create({
+        userId:userId,
+        title: title,
+        author: author,
+        publisher: publisher,
+        totalPrice: totalPrice,
+        isbn: isbn
+    });
+    return newBook;
+}
+
+
+async function getBook(userId,title) {
+    return await Book.findOne({ "title": title,"userId":userId });
+}
+
+
+async function updateBook(userId,id, updatedFields) {
+    const book = await Book.updateOne({ "_id": id,"userId": userId }, { $set: updatedFields });
+    return book;
+}
+
+
+async function deleteBook(userId,id) {
+    await Book.deleteOne({ "_id": id,"userId": userId });
+}
+
+
+module.exports = { createBook, getBook, updateBook, deleteBook };
