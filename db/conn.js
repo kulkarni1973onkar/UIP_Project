b1090e40fc0abb0d56391728a74b1f338e5d3111
Onkar Kulkarni <91174083+kulkarni1973onkar@users.noreply.github.com>
2024-06-15 00:32:11 +0530

    Updated commit

--- db/conn.js
+++ db/conn.js
@@ -1,13 +1,39 @@
-const mongoose = require("mongoose");
+const { MongoClient } = require('mongodb');
 
-mongoose.connect("mongodb://localhost:27017/BooksBuzz", {
-userNewUrlParser:true,
-useUnifiedTopology:true,
-useCreateIndex:true
 
-}).then(() => {
+const uri = 'mongodb://localhost:27017'; 
 
-    console.log('connection successful');
-}).catch((e) => {
-    console.log('no connection')
-})
+
+const dbName = 'BooksBuzz'; 
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function connectToMongoDB() {
+    try {
+        
+        await client.connect();
+        console.log('Connected successfully to MongoDB server');
+
+        
+        const db = client.db(dbName);
+        
+        
+
+    ClientSession
+        const collection = db.collection('mycollection');
+        const query = {};
+        const result = await collection.find(query).toArray();
+        console.log('Query result:', result);
+
+    } catch (err) {
+        console.error('Error connecting to MongoDB:', err);
+    } finally {
+        
+        await client.close();
+        console.log('MongoDB client closed');
+    }
+}
+
+
+connectToMongoDB();
