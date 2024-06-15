fba3b512c35b732ba18f048db0236fc781533668
Onkar Kulkarni <91174083+kulkarni1973onkar@users.noreply.github.com>
2024-06-15 08:51:46 +0530

    Updated User.js

--- Server/Models/User.js
+++ Server/Models/User.js
@@ -1,31 +1,48 @@
+// 1. 
 const mongoose = require("mongoose");
 
+// 2.
 const userSchema = new mongoose.Schema({
 
     name: String,
     username: String,
     password: String,
+    confirmpassword:String,
     email: String,
     address:String,
-    followers:[String],
-    following:[String]
-
+    mobileno:[String]
+    
 
 })
-
+// 3.
 const User = mongoose.model("User",userSchema);
-//Register & Create
-async function register(username,password)
+
+// 4.
+async function register(name,username,password,confirmpassword,email,address,mobileno)
 {
     const user = getUser(username);
     if(user) throw Error('Username already Exists');
 
     const newUser = await User.create({
+        name:name,
         username: username,
-        password: password
+        password: password,
+        confirmpassword:confirmpassword,
+        email:email,
+        address:address,
+        mobileno:mobileno
     });
 
     return newUser
+}
+
+// GET Users
+async function getAllUsers() {
+    const database = client.db('test');
+    const collection = database.collection('users');
+    const users = await collection.find().toArray()
+    console.log(users);
+    return users;
 }
 //Login & Read
 async function login(username,password)
@@ -52,6 +69,7 @@ async function deleteUser(id)
     await user.deleteOne({"_id":id});
 };
 
+// Utility function
 async function getUser(username)
 {
     return await User.findOne({"username":username});
@@ -59,4 +77,4 @@ async function getUser(username)
 
 //Export
 
-module.exports ={register,login,updatePassword,deleteUser};
+module.exports ={register,getAllUsers,login,updatePassword,deleteUser};
